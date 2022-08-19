c503f5f63f3f0dbd1ddf8a0dbf63f54da95bd56d
«Vitaliy <remotealex@yandex.ru>
2022-08-19 20:02:36 +0300

    Задание 3

--- Lesson1/ДЗ3/Program.cs
+++ Lesson1/ДЗ3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear ();
+Console.Write ("Введите любое целое число  ");
+int number = int.Parse (Console.ReadLine());
+int result = number % 2;
+if (result > 0)
+{
+Console.WriteLine("Это нечётное число");  
+}
+else
+{
+Console.WriteLine("Это чётное число");  
+}
