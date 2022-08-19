7d6ea8160e934b436056ff64e3a3a2e05aa950e5
«Vitaliy <remotealex@yandex.ru>
2022-08-19 19:36:34 +0300

    Вторая задача

---
+++ Lesson1/ДЗ2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear ();
+Console.Write ("Введите первое число  ");
+int number1 = int.Parse (Console.ReadLine());
+Console.Write ("Введите второе число  ");
+int number2 = int.Parse (Console.ReadLine());
+Console.Write ("Введите третье число  ");
+int number3 = int.Parse (Console.ReadLine());
+int maximum;
+if (number1<number2)
+{
+maximum = number2;
+}
+else
+{
+maximum = number1;
+}
+if (maximum<number3)
+{
+maximum = number3;
+}
+else
+{
+maximum = maximum;
+}
+Console.WriteLine($"Большим числом является {maximum}");
