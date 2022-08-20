2c4568ce63c190ddf06720020ca356f6f43dae43
«Vitaliy <remotealex@yandex.ru>
2022-08-20 12:37:00 +0300

    Задание 4

---
+++ Lesson1/ДЗ4/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear ();
+Console.Write ("Введите любое целое число  ");
+int N = int.Parse (Console.ReadLine());
+for (int step = 2; step < N; step = step + 2)
+{
+    Console.WriteLine(step);
+}
